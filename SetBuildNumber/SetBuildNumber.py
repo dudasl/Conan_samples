9cab832eb3d00b7754a5e6ab8756d23f79b1e9d9
Ladislav Dudas <ladislav_dudas@foxitsoftware.com>
2020-12-01 20:34:25 +0100

    Support utf-16-le and utf-16-be encoding

--- SetBuildNumber/SetBuildNumber.py
+++ SetBuildNumber/SetBuildNumber.py
@@ -1,10 +1,27 @@
 import sys
+import codecs
+import encodings
+
+def get_encoding(file):
+    byte_string = file.read(2)
+    if byte_string.startswith(codecs.BOM_UTF16_LE):
+        return 'utf_16_le'
+    if byte_string.startswith(codecs.BOM_UTF16_BE):
+        return 'utf_16_be'
+
+    return 'ascii'
 
 def set_build_number(file_path, build_version):
+    # Read byte BOM and get propriate encoding
+    encoding = None
+    with open(file_path, "rb") as file:
+        encoding = get_encoding(file)
+
     # Read all lines from version.txt
-    file = open(file_path, 'r') 
-    lines = file.readlines()  
-    file.close()
+    with codecs.open(file_path, 'r', encoding=encoding) as file:
+        if encoding == 'utf_16_le' or encoding == 'utf_16_be':
+            file.read(1)
+        lines = file.readlines()
 
     # Modify version number
     for i in range(len(lines)):
@@ -14,20 +31,24 @@ def set_build_number(file_path, build_version):
         
         version = line[9:]
         version_list = version.split('.')
-        if len(version_list) != 4:
+        if len(version_list) < 4:
             continue
 
         major = int(version_list[0])
         minor = int(version_list[1])
         patch = int(version_list[2])
 
-        line = 'version: ' + str(major) + '.' + str(minor) + '.' + str(patch) + '.' + str(build_version)
+        line = 'version: ' + str(major) + '.' + str(minor) + '.' + str(patch) + '.' + str(build_version) + '\r\n'
         lines[i] = line
 
     # Write all lines to version.txt
-    file = open(file_path, 'w') 
-    file.writelines(lines)
-    file.close()
+    with codecs.open(file_path, 'w', encoding=encoding) as file:
+        if encoding == 'utf_16_le':
+            file.write('\ufeff')
+        elif encoding == 'utf_16_be':
+            file.write('\ufeff')
+        for line in lines:
+            file.write(line)
 
 if __name__ == "__main__":
    set_build_number(sys.argv[1], sys.argv[2])
